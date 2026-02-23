88071026adb11e497fa9c433120def56c3ea8306
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2026-02-23 14:12:34 +0530

    Add files via upload

---
+++ day23.c
@@ -0,0 +1,87 @@
+//Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definition
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Merge two sorted lists
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+
+    if (l1 == NULL) return l2;
+    if (l2 == NULL) return l1;
+
+    struct Node* result = NULL;
+
+    if (l1->data <= l2->data) {
+        result = l1;
+        result->next = mergeLists(l1->next, l2);
+    } else {
+        result = l2;
+        result->next = mergeLists(l1, l2->next);
+    }
+
+    return result;
+}
+
+// Print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m, value;
+    struct Node *list1 = NULL, *list2 = NULL, *merged = NULL;
+
+    // First list
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        list1 = insertEnd(list1, value);
+    }
+
+    // Second list
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &value);
+        list2 = insertEnd(list2, value);
+    }
+
+    // Merge lists
+    merged = mergeLists(list1, list2);
+
+    // Print merged list
+    printList(merged);
+
+    return 0;
+}
