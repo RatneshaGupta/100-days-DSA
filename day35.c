a2834e47b7218730d86e012c60eb2ef8e1e68d6a
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2026-03-09 13:45:29 +0530

    Add files via upload

---
+++ day35.c
@@ -0,0 +1,55 @@
+//Problem: Queue Using Array - Implement using linked list with dynamic memory allocation.
+
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+// Enqueue operation
+void enqueue(int value)
+{
+    if(rear == MAX - 1)
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if(front == -1)
+        front = 0;
+
+    rear++;
+    queue[rear] = value;
+}
+
+// Display queue elements
+void display()
+{
+    if(front == -1)
+    {
+        printf("Queue is empty");
+        return;
+    }
+
+    for(int i = front; i <= rear; i++)
+    {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main()
+{
+    int n, x;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
