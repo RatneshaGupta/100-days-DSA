cbf56358bb2a917c556bdbd72d6b6aed25e3215d
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2026-04-17 19:47:20 +0530

    Add files via upload

---
+++ day76.c
@@ -0,0 +1,54 @@
+//Problem Statement Using DFS or BFS, count number of connected components.
+
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];   // Adjacency matrix
+int visited[MAX];
+
+void dfs(int node, int n) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;  // undirected graph
+    }
+
+    int components = 0;
+
+    // Count components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i, n);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
+
